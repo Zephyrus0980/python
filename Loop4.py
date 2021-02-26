eb02145dd021d4ac6efeb95094f64375a7880723
Zephyrus0980 <79694879+Zephyrus0980@users.noreply.github.com>
2021-02-26 20:59:38 +0800

    0226-1

---
+++ Loop4.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+說明巢狀迴圈
+
+
+
+
+"""
+
+
+
+
+
+#for i in range(1,5):#=>範圍=>[1,2,3,4]
+#    for a in range(3):#=>[0,1,2]
+#        print(i,end='')
+#    print()
+#
+#print("程式執行完畢")
+
+
+
+
+#for i in range(1,5):#=>範圍=>[1,2,3,4]
+#    for a in range(3):#=>[0,1,2]
+#        print(a,end='')
+#    print()
+#
+#print("程式執行完畢")
+
+
+
+#for i in range(1,5):#=>範圍=>[1,2,3,4]
+#    for a in range(i):#=>[0,1,2]
+#        print(a,end='')
+#    print()
+#
+#print("程式執行完畢")
+
+"""
+作業2
+"""
+
+for i in range(1,5):#=>範圍=>[1,2,3,4]
+    for a in range(i):#=>[0,1,2]
+        print(a+1,end='')
+    print()
+
+print("程式執行完畢")
+
+
+
+#for i in range(1,5):#=>範圍=>[1,2,3,4]
+#    for a in range(i,a+1):#=>[0,1,2]
+#        print(a,end='')
+#    print()
+#
+#print("程式執行完畢")
